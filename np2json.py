05a7d860528db912e0d745750bae47a8fa4ffc9c
sungjun4257 <79438351+sungjun4257@users.noreply.github.com>
2023-04-20 00:18:30 +0900

    Update np2json.py
    code summarize

--- np2json.py
+++ np2json.py
@@ -1,12 +1,29 @@
 ## Version 2.0
-## update : 2023.04.19
+## update : 2023.04.20
 
 import numpy as np
 import json
 import os
 from collections import OrderedDict
 
-def closest_point_2_lines(oa, da, ob, db): # returns point closest to both rays of form o+t*d, and a weight factor that goes to 0 if the lines are parallel
+#np.set_printoptions(threshold=np.inf,linewidth=np.inf)
+
+############# Please change and check the File Path ###################
+
+file_path="./llff.json"               #json file path
+np_path="./barf_output_pose.npy"      #numpy file(barf output) path
+img_path="./llff/images"              #dataset image file path
+
+#############################################################
+
+img_list=sorted(os.listdir(img_path)) # load image name list
+
+path=['./llff/images/']               # make image_path like json file
+for i in range(0,len(img_list)): 
+   img_list[i]=path[0]+img_list[i]
+
+
+def closest_point_2_lines(oa, da, ob, db): 
 	da = da / np.linalg.norm(da)
 	db = db / np.linalg.norm(db)
 	c = np.cross(da, db)
@@ -19,6 +36,7 @@ def closest_point_2_lines(oa, da, ob, db): # returns point closest to both rays 
 	if tb > 0:
 		tb = 0
 	return (oa+ta*da+ob+tb*db) * 0.5, denom
+# returns point closest to both rays of form o+t*d, and a weight factor that goes to 0 if the lines are parallel
 
 def rotmat(a, b):
 	a, b = a / np.linalg.norm(a), b / np.linalg.norm(b)
@@ -31,32 +49,22 @@ def rotmat(a, b):
 	kmat = np.array([[0, -v[2], v[1]], [v[2], 0, -v[0]], [-v[1], v[0], 0]])
 	return np.eye(3) + kmat + kmat.dot(kmat) * ((1 - c) / (s ** 2 + 1e-10))
 
-#np.set_printoptions(threshold=np.inf,linewidth=np.inf)
-file_path="./llff.json"
-np_path="./barf_ouput_pose.npy"
-img_path="./llff/images"
-path=['./llff/images/']
-pose_path="./poses_bounds.npy"
-img_list=sorted(os.listdir(img_path))
 
 
-list_homo=[0.0 , 0.0 , 0.0 , 1.0]
-
-np_data=np.load(np_path)
+np_data=np.load(np_path)            # load numpy file
 print(np_data.shape)
-np_data=np_data.tolist()  # numpy to list
+np_data=np_data.tolist()            # numpy to list
+ 
 
-for i in range(0,len(np_data)):	 # make homogeneous matrix
+list_homo=[0.0 , 0.0 , 0.0 , 1.0]   # make homogeneous matrix
+for i in range(0,len(np_data)):	 
    np_data[i].append(list_homo)
 
-np_data=np.array(np_data) # list to numpy
+np_data=np.array(np_data)           # list to numpy
 
 
-with open(file_path,'r') as file: # open json file
+with open(file_path,'r') as file:   # open json file
    data=json.load(file)
-
-for i in range(0,len(img_list)): # make image_path like json file
-   img_list[i]=path[0]+img_list[i]
 
 
 up = np.zeros(3)
@@ -66,15 +74,15 @@ np_data=np.linalg.inv(np_data)
 for change_num in range(0,len(img_list)):
    for i in data["frames"]:   
       if i["file_path"] == img_list[change_num]:
-         np_data[change_num][0:3,2] *= -1 # flip the y and z axis
+         np_data[change_num][0:3,2] *= -1    # flip the y and z axis
          np_data[change_num][0:3,1] *= -1
          np_data[change_num]=np_data[change_num][[1,0,2,3],:]
-         np_data[change_num][2,:] *= -1  # flip whole world upside down
+         np_data[change_num][2,:] *= -1     # flip whole world upside down
          up += np_data[change_num][0:3,1]
          print("change num", change_num)
          print("up ",up)
 
-for change_num in range(0,len(img_list)):
+for change_num in range(0,len(img_list)):   # change original json file data
    for i in data["frames"]:   
       if i["file_path"] == img_list[change_num]:
          i["transform_matrix"][0]=np_data[change_num][0]
@@ -82,7 +90,7 @@ for change_num in range(0,len(img_list)):
          i["transform_matrix"][2]=np_data[change_num][2]
          i["transform_matrix"][3]=np_data[change_num][3]
 
-print("change transformation")
+
 up = up / np.linalg.norm(up)
 print("up vector was", up)
 R = rotmat(up,[0,0,1]) # rotate up vector to [0,0,1]
@@ -128,5 +136,9 @@ print(nframes,"frames")
 
 with open(file_path,'w') as make_file: # overwrite json
    json.dump(data,make_file,indent=2)
+
+
+
+
 
 print("complete!")
